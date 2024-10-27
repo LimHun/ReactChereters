26429ad0a59cf00c8855fdfa35135c942b52fb8d
임훈 <dlagnskkld3@gmail.com>
2024-10-27 23:30:30 +0900

    init project

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { createGlobalStyle } from "styled-components";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #1e1e1e;
+    color: white;
+    margin: 0;
+    font-family: Arial, sans-serif;
+  }
+`;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const queryClient = new QueryClient();
+
+ReactDOM.render(
+	<QueryClientProvider client={queryClient}>
+		<BrowserRouter>
+			<GlobalStyle />
+			<App />
+		</BrowserRouter>
+	</QueryClientProvider>,
+	document.getElementById("root")
+);
